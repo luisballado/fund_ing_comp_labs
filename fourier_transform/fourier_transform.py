c25ae4d933d945dc1e238528d0cb1248c335b4a2
Luis Ballado <luis@madlab.mx>
2022-11-15 17:48:45 -0600

    grafias

--- fourier_transform/fourier_transform.py
+++ fourier_transform/fourier_transform.py
@@ -3,15 +3,15 @@ import math
 import matplotlib.pyplot as plt
 
 _x_ = np.linspace(-2,2,10000)
+f = []
 
-T = 4
-
-f=[]
-
+#iterar para los valores de t
 for i in _x_:
     w = 2*np.pi*i
-    f.append(abs((2/w)*(np.cos(2*w)-np.cos(w))))
-
+    #f.append(abs((2/w)*(np.cos(2*w)-np.cos(w)))) #Funcion1
+    #f.append(abs((np.exp(-1j*w)*(-1j*w+np.exp(1j*w)-1))/((1j*w)*(1j*w)))) #Funcion2
+    f.append(abs((2-np.exp(-1j*w)-np.exp(-2j*w))/(1j*w))) #Funcion3
+    
 plt.plot(_x_,f,color="dodgerblue")
-plt.title("Transformada de Fourier Funcion 1")
-plt.savefig("trans_fourier.png")
+plt.title("Transformada de Fourier Funcion 3")
+plt.savefig("trans_fourier_3.png")
