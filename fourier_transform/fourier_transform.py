efcb602663323d8a2e96c2fc6fdb450550837b41
Luis Alberto Ballado Aradias <luisalbertoballadoaradias@iMac-de-LTI-9-Luis.local>
2022-11-15 23:38:17 -0600

    tarea2

--- fourier_transform/fourier_transform.py
+++ fourier_transform/fourier_transform.py
@@ -9,9 +9,10 @@ f = []
 for i in _x_:
     w = 2*np.pi*i
     #f.append(abs((2/w)*(np.cos(2*w)-np.cos(w)))) #Funcion1
-    #f.append(abs((np.exp(-1j*w)*(-1j*w+np.exp(1j*w)-1))/((1j*w)*(1j*w)))) #Funcion2
-    f.append(abs((2-np.exp(-1j*w)-np.exp(-2j*w))/(1j*w))) #Funcion3
+    f.append(abs((np.exp(-1j*w)*(-1j*w+np.exp(1j*w)-1))/((1j*w)*(1j*w)))) #Funcion2
+    #f.append(abs((2-np.exp(-1j*w)-np.exp(-2j*w))/(1j*w))) #Funcion3
     
 plt.plot(_x_,f,color="dodgerblue")
 plt.title("Transformada de Fourier Funcion 3")
+plt.show()
 plt.savefig("trans_fourier_3.png")
