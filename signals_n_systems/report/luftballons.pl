81182eec817caae9de317e4dfcf54439bf3441b6
Luis Ballado <luis@madlab.mx>
2023-01-02 04:44:25 -0600

    reporte

--- signals_n_systems/report/luftballons.pl
+++ signals_n_systems/report/luftballons.pl
@@ -1,22 +1,57 @@
-#!/usr/bin/perl 
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import re
 
-use strict; 
-use warnings;
+def u(t):
+    """Unit step function"""
+    return 1 * (t >= 0)
 
-for (1..99) { print $_." Luftballons\n"; }
+#definicion delta function
+def imp(t):
+    return ((t)==0)*1.0
 
-# This is a commented line
+n = np.arange(-10,10,1)
 
-my $string = "Hello World!";
+regex = r'x\[n\]='
+i = 0
+with open(sys.argv[1],'r',encoding='utf-8') as file:
+    for line in file:
+        #print("calcular")
+        if re.search(regex,line):
+            eq = line.split("=",1)[1]            
+            #reemplazar el -1^n por cos(n*pi)
+            if re.search(r'-1\^n',eq):
+                
+                new_string = eq.replace("-1^n","np.cos(n*np.pi)").replace("[","(").replace("]",")")
+                
+                x = new_string.replace("\n", "")
+                plt.figure(i)
+                plt.stem(n, eval(x))
+                plt.grid()
+                plt.savefig('graph'+str(i)+'.png')
+                
+            elif re.search(r'sigma',eq):
+                txt = re.search(r'\((.*?)\)',eq).group(1)
+                x = txt.split(",")
+                g = np.array([0])
 
-print $string."\n\n";
-
-$string =~ s/Hello/Goodbye Cruel/;
-
-print $string."\n\n";
-
-finale();
-
-exit;
-
-sub finale { print "Fin.\n"; }
+                for k in range(int(x[1])):
+                    
+                    data = -3*k-1
+                    g = g + 4*imp(n+data)
+    
+                plt.figure(2)
+                plt.stem(n,g)
+                plt.grid()
+                plt.savefig('graph.png')
+                
+            else:
+                new_string = eq.replace("[","(").replace("]",")")
+                x = new_string.replace("\n", "")
+                plt.figure(i)
+                plt.stem(n, eval(x))
+                plt.grid()
+                plt.savefig('graph'+str(i)+'.png')
+                
+            i = i + 1
